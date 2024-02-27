c6cf61e68acc4a729daa5697f39eba639c12fd3c
Andrew Chung <andrewchung@DN0a1f2f1b.SUNet>
2024-02-26 23:24:02 -0800

    intiial'

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [output, setOutput] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const endpoint = 'http://54.218.124.218:5000/query'
+    const payload = { message: inputValue };
+
+    try {
+        const response = await fetch(endpoint, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(payload),
+        });
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        setOutput(data.message);
+        console.log("Response data:", data);
+      } 
+    catch (error) {
+        console.error("Error fetching data:", error);
+    } 
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="textInput">Search for mentor:</label>
+        <input
+          type="text"
+          id="textInput"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      <p>output</p>
     </div>
   );
 }
