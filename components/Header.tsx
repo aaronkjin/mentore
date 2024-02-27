3532ec57ab304e67211a0cf9c6ea561c55dcbcbe
aaronkjin <58490258+hoxykidmilli@users.noreply.github.com>
2024-02-26 22:49:45 -0800

    revamp

--- components/Header.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "../styles/Home.module.css";
-
-const Header = () => (
-  <header className="absolute top-0 left-0 p-4 text-2xl text-gray-400">
-    Mentore
-  </header>
-);
-
-export default Header;
