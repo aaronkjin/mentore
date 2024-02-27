30266fa6d086a8134d15182c414421e0d9980bc8
aaronkjin <58490258+hoxykidmilli@users.noreply.github.com>
2024-02-26 23:28:26 -0800

    styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,34 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [inputValue, setInputValue] = useState('');
-  const [output, setOutput] = useState('');
+  const [inputValue, setInputValue] = useState("");
+  const [output, setOutput] = useState("");
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    const endpoint = 'http://54.218.124.218:5000/query'
+    const endpoint = "http://54.218.124.218:5000/query";
     const payload = { message: inputValue };
 
     try {
-        const response = await fetch(endpoint, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(payload),
-        });
+      const response = await fetch(endpoint, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payload),
+      });
 
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
 
-        const data = await response.json();
-        setOutput(data.message);
-        console.log("Response data:", data);
-      } 
-    catch (error) {
-        console.error("Error fetching data:", error);
-    } 
+      const data = await response.json();
+      setOutput(data.message);
+      console.log("Response data:", data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
   };
 
   return (
